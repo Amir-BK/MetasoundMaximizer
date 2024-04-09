2f78a53eac7f82e4c827f7f963b74b8df8e499fe
Ishay Tubi <ishay@neuron.vision>
2024-04-09 11:56:56 +0300

    Linear fader input gain as Dry/Wet

--- Source/TKMaximizer/Private/SignalProcessing/EnhancedDynamics.cpp
+++ Source/TKMaximizer/Private/SignalProcessing/EnhancedDynamics.cpp
@@ -12,9 +12,9 @@ void FEnhancedDynamicsProcessor::ProcessAudio(const FAudioBufferReadRefArray &In
     for (int32 sample_index=0; sample_index < InNumSamples; sample_index++){
         
         //Calc max(abs(each channel))
-        float max_abs = fabsf(Inputs[0]->GetData()[sample_index] * InputGain);
+        float max_abs = fabsf(Inputs[0]->GetData()[sample_index]);
         for (int32 channel_index=0; channel_index < GetNumChannels(); channel_index++){
-            const float sample = fabsf(Inputs[channel_index]->GetData()[sample_index] * InputGain);
+            const float sample = fabsf(Inputs[channel_index]->GetData()[sample_index]);
             max_abs = fmaxf(max_abs, sample);
         }
         
@@ -27,12 +27,27 @@ void FEnhancedDynamicsProcessor::ProcessAudio(const FAudioBufferReadRefArray &In
         } else {
             env_state = env_state * release + (1-release) * max_abs;
         }
+        const float curve_value = 4;
+        const float max_curve_value = atanf(curve_value + 0.01f);  // The maximum value possible for input values 0..1.
+        float env_after_fitting_curve = atanf(curve_value * env_state + 0.01f); // This also should be a parameter.
+        //Normlize to 0..1
+        env_after_fitting_curve /= max_curve_value;
         
-        float gain_fix = atanf(3.0f * env_state + 0.01f); // This also should be a paramter.
+        //Use Input gain as linear factor from linear to curve
+        env_after_fitting_curve = InputGain * env_after_fitting_curve + (1.0f - InputGain) * 1.0;
+        //Smooth running gain
+        if (env_after_fitting_curve<running_gain_state) {
+            //Want to lower the gain?
+            running_gain_state = running_gain_state * attack + (1.0f - attack) * env_after_fitting_curve;
+        } else{
+            running_gain_state = running_gain_state * release + (1.0f - release) * env_after_fitting_curve;
+        }
         
-        
+        //float interploated_value = 1.0f / (InputGain * gain_fix + (1.0f - InputGain) * 1.0);
+        //Clamp
+        //interploated_value = fminf(interploated_value, )
         for (int32 channel_index=0; channel_index < GetNumChannels(); channel_index++){
-            OutBuffer[channel_index]->GetData()[sample_index] = InputGain * Inputs[channel_index]->GetData()[sample_index] / gain_fix ;
+            OutBuffer[channel_index]->GetData()[sample_index] = running_gain_state * Inputs[channel_index]->GetData()[sample_index]  ;
         }
     }
     
--- Source/TKMaximizer/Public/SignalProcessing/EnhancedDynamics.h
+++ Source/TKMaximizer/Public/SignalProcessing/EnhancedDynamics.h
@@ -14,7 +14,8 @@ class FEnhancedDynamicsProcessor : public Audio::FDynamicsProcessor
 {
     
 public:
-    float env_state = 0.0;
+    float env_state = 0.0f;
+    float running_gain_state = 1.0f;
     void ProcessAudioFrame(const float* InFrame, float* OutFrame, const float* InKeyFrame);
     void ProcessAudio(const FAudioBufferReadRefArray &Inputs, FAudioBufferRWriteRefArray &OutBuffer);
     
